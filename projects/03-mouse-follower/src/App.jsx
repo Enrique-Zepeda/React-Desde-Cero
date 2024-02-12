2adf48cd06ec372663ffd6e1d7baa4f35bf1956b
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-02-11 21:25:31 -0600

    Imprimiendo posicion del mouse

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,19 +1,39 @@
 import { useEffect, useState } from "react"
 
+
 function App() {
 
   const [enabled, setEnabled] = useState(false)
 
 useEffect(() => {
-  console.log("Efecto")
-})
+  console.log("Efecto" , {enabled})
+  const handleMove = (event) => {
+    const { clientX, clientY } = event
+    console.log("handleMove", {clientX, clientY})
+  }
+
+  window.addEventListener('pointermove', handleMove)
+},[enabled])
 
 
   return (
-    <>
-      <h3>Proyecto 3</h3>
+    <main>
+        <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        // transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
       <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} Activar Seguir Puntero</button>
-    </>
+    </main>
   )
 }
 
