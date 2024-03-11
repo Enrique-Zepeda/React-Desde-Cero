ac78fd098dcaef656eddf1a2618060a22e88c016
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-11 14:23:09 -0600

    Usando useRef para guardar el input

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,15 +1,26 @@
+import { useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 
 function App() {
   const { movies } = useMovies();
+  const inputRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = inputRef.current.value;
+    console.log(value);
+  };
   return (
     <div className="page">
       <h1>Buscador peliculas</h1>
       <header>
-        <form>
-          <input type="text" placeholder="Avengers, Star Wars..." />
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Avengers, Star Wars..."
+          />
           <button type="submit">Buscar</button>
           {/*Siempre el boton de un formulario es tipo submit*/}
         </form>
