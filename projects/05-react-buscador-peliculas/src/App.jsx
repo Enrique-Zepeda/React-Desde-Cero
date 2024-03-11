143e7204566388beec7a075fde592011b4dea205
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-11 08:34:18 -0600

    Mostrando las peliculas con el json de mocks

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
+import withoutResponse from "./mocks/no-results.json";
+import responseMovie from "./mocks/with-results.json";
 function App() {
+  const movies = responseMovie.Search; //guardo el arreglo en movies
+  const hasMovies = movies?.length > 0; //verifico si tiene peliculas
   return (
     <div className="page">
       <h1>Buscador peliculas</h1>
@@ -10,7 +14,21 @@ function App() {
           {/*Siempre el boton de un formulario es tipo submit*/}
         </form>
       </header>
-      <main>Aqui iran los resultados</main>
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No existe la pelicula buscada</p>
+        )}
+      </main>
     </div>
   );
 }
