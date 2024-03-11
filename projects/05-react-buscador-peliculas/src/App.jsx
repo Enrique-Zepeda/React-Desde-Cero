d455b3d53ed7f5f63dc41f960729494b5a9d662d
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-11 08:08:50 -0600

    estilos basicos

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,8 +1,17 @@
+import "./App.css";
 function App() {
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+    <div className="page">
+      <h1>Buscador peliculas</h1>
+      <header>
+        <form>
+          <input type="text" placeholder="Avengers, Star Wars..." />
+          <button type="submit">Buscar</button>{" "}
+          {/*Siempre el boton de un formulario es tipo submit*/}
+        </form>
+      </header>
+      <main>Aqui iran los resultados</main>
+    </div>
   );
 }
 
