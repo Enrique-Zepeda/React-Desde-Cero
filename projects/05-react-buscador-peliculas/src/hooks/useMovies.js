d8b2aba5afa0b58100426685d26ac7fe02610c27
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-14 16:26:02 -0600

    usando useRef para evitar buscar lo mismo dos veces

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { searchMovies } from "../services/movies.js";
 
 export const useMovies = ({ search }) => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const previousSearch = useRef(search);
   const getMovies = async () => {
+    if (search === previousSearch.current) return;
     try {
       setIsLoading(true);
       setError(null);
+      previousSearch.current = search;
       const newMovies = await searchMovies({ search });
       setMovies(newMovies);
     } catch (error) {
