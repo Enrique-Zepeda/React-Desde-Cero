96e5da70e6d4ed2677ae41c9d9ae20b45b851c92
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-15 09:08:28 -0600

    usando useMemo en getMovies

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -6,21 +6,24 @@ export const useMovies = ({ search, sort }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
-  const getMovies = async () => {
-    if (search === previousSearch.current) return;
-    try {
-      setIsLoading(true);
-      setError(null);
-      previousSearch.current = search;
-      const newMovies = await searchMovies({ search });
-      setMovies(newMovies);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      //tanto si se ejecuta el try o el catch el finally siempre se va a ejecutar
-      setIsLoading(false);
-    }
-  };
+
+  const getMovies = useMemo(() => {
+    return async ({ search }) => {
+      if (search === previousSearch.current) return;
+      try {
+        setIsLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        //tanto si se ejecuta el try o el catch el finally siempre se va a ejecutar
+        setIsLoading(false);
+      }
+    };
+  }, []);
 
   const sortedMovies = useMemo(() => {
     console.log("memo sorted movies");
