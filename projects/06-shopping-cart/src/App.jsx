4f984735c8cc4335c19f685f2e679762ad8babea
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-28 16:55:00 -0600

    Actualizando mockup y mostrando imagenes de carritos

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,7 +1,11 @@
+import { products } from "./mocks/products.json";
+import { Products } from "./components/Products";
+
 function App() {
   return (
     <>
       <h1>Shopping Card 🛒</h1>
+      <Products products={products} />
     </>
   );
 }
