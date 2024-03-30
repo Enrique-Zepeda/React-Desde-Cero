0c6e54ade05b0cc5bf012ffdd86bc8ababb37580
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-03-30 09:17:57 -0600

    Agregando rango en el filtro

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import "../styles/Filters.css";
 
 export const Filters = () => {
+  const [rango, setRango] = useState(0);
+
+  const handleRango = (e) => {
+    console.log(e.target.value);
+    const data = e.target.value;
+    setRango(data);
+  };
+
   return (
     <section className="filters">
       <div>
-        <label htmlFor="price">Price</label>
-        <input type="range" id="price" min="0" max="2000" />
+        <label htmlFor="price">Precio</label>
+        <input
+          type="range"
+          id="price"
+          min="0"
+          max="2000"
+          onChange={handleRango}
+        />
+        <p>{rango}</p>
       </div>
 
       <div>
