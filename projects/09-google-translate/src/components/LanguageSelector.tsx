3a010cd3b4409bdcefcd856325366b6de09028d3
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-22 06:06:24 -0600

    agregando funcionalidad y tipando

--- projects/09-google-translate/src/components/LanguageSelector.tsx
+++ projects/09-google-translate/src/components/LanguageSelector.tsx
@@ -1,9 +1,25 @@
 import Form from "react-bootstrap/Form";
 import { SUPPORTED_LANGUAGES } from "../constants";
+import { FromLanguage, type Language } from "../types";
 
-export function LanguageSelector({ onChange }) {
+type Props =
+  | {
+      type: "from";
+      value: FromLanguage;
+      onChange: (language: FromLanguage) => void;
+    }
+  | { type: "to"; value: Language; onChange: (language: Language) => void };
+
+export function LanguageSelector({ onChange, type, value }: Props) {
+  const hancleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(event.target.value as Language);
+  };
   return (
-    <Form.Select aria-label="Seleciona un idioma">
+    <Form.Select
+      aria-label="Seleciona un idioma"
+      onChange={hancleChange}
+      value={value}
+    >
       {Object.entries(SUPPORTED_LANGUAGES).map(([key, literal]) => (
         <option key={key} value={key}>
           {literal}
